1dea2a081ef725c618158d75cb24d0a22ab14f2d
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-07-17 02:14:27 +0800

    created a better error object for validation

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import { AnyZodObject, ZodError, ZodIssue } from 'zod';
+import { AnyZodObject, z, ZodError } from 'zod';
 import logger from '../utils/logger';
+
+type ActionErrors<T> = Partial<Record<keyof T, string>>;
 
 const validate =
   (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
@@ -10,12 +12,20 @@ const validate =
     } catch (error) {
       logger.error(error);
       if (error instanceof ZodError) {
-        return res.status(400).json(
-          error.flatten((issue: ZodIssue) => ({
-            message: issue.message,
-            errorCode: issue.code,
-          })).fieldErrors
+        const errors = error as ZodError<z.TypeOf<typeof schema>>;
+
+        // tom's version
+        const tomErrors = errors.issues.reduce(
+          (acc: ActionErrors<z.TypeOf<typeof schema>>, curr) => {
+            const key = curr.path[0] as keyof z.TypeOf<typeof schema>;
+            acc[key] = curr.message;
+
+            return acc;
+          },
+          {}
         );
+
+        return res.status(400).json(tomErrors);
       }
       return res.status(500).json({ error: 'something went wrong' });
     }
